d099f31d5b6aa5ae779e319d4af0914d16b66ff1
Верховод Никита Сергеевич <nsverhovod@mts.ru>
2025-01-15 20:49:44 +0300

    adding practise-1

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    kotlin("jvm")
+}
+
+group = "studying"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.36")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.36")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
