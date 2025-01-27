db7afdab95f25b08682e42f84b9b9e1b0e9bea94
Верховод Никита Сергеевич <nsverhovod@mts.ru>
2025-01-27 14:41:41 +0300

    add practise-3 template

---
+++ practice/practise-3/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+	java
+	checkstyle
+	id("org.springframework.boot") version "3.4.2"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "hse"
+version = "0.0.1-SNAPSHOT"
+
+checkstyle {
+	toolVersion = "10.13.0"
+	isIgnoreFailures = false
+	maxWarnings = 0
+	maxErrors = 0
+}
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
