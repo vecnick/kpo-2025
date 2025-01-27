5c5a5f2f7687964eedf94a11697ac6c5d956a133
Верховод Никита Сергеевич <nsverhovod@mts.ru>
2025-01-27 16:13:01 +0300

    fix README.md

--- practice/practise-3/build.gradle.kts
+++ practice/practise-3/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
