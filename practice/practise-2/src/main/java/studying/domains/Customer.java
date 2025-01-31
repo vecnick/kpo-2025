628877131077da73d9c651694a0129377af20241
maksim0840 <maksim0840@gmail.com>
2025-01-31 14:54:02 +0300

    add Javadoc

--- practice/practise-2/src/main/java/studying/domains/Customer.java
+++ practice/practise-2/src/main/java/studying/domains/Customer.java
@@ -4,6 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * Класс покупателя
+ * имеет ФИО (name), силу ног (legPower), силу рук (handPower), интеллект (IQ)
+ * имеет привязку к своей машине (car)
+ */
 @Getter
 @ToString
 public class Customer {
@@ -18,11 +23,19 @@ public class Customer {
     @Setter
     private Car car;
 
+    /**
+     * Конструктор для создания покупателя, задающегося параметрами
+     *
+     * @param name - ФИО
+     * @param legPower - сила ног
+     * @param handPower - сила рук
+     * @param IQ - интеллект
+     */
     public Customer(String name, int legPower, int handPower, int IQ) {
         this.name = name;
         this.legPower = legPower;
         this.handPower = handPower;
         this.IQ = IQ;
     }
-    
+
 }
