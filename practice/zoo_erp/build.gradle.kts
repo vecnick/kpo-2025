8a92f528c2cb3f27710b028e6d73a52a8ff73bee
maksim0840 <maksim0840@gmail.com>
2025-02-14 10:41:16 +0300

    minihw1

---
+++ practice/zoo_erp/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+	java
+	jacoco
+	id("org.springframework.boot") version "3.4.2"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "erp"
+version = "1.0.0"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven("https://jitpack.io") // для java-object-diff
+}
+
+dependencies {
+	implementation("org.apache.commons:commons-lang3:3.12.0")
+	implementation("de.danielbechler:java-object-diff:0.93.1")
+	implementation("org.springframework.boot:spring-boot-starter")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.test {
+	finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+	dependsOn(tasks.test) // tests are required to run before generating the report
+}
