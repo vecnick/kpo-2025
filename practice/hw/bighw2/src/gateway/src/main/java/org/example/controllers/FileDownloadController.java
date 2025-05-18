9ffa6588a5dc1c8aee37015167a8734c24b11d06
hzhdlrp <pswjft@gmail.com>
2025-05-18 19:51:39 +0300

    small changes

---
+++ practice/hw/bighw2/src/gateway/src/main/java/org/example/controllers/FileDownloadController.java
@@ -0,0 +1,41 @@
+package org.example.controllers;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/upload")
+@Tag(name = "Скачивание файлов", description = "Скачивание файлов по ID")
+public class FileDownloadController {
+
+    @Value("http://storage:8081")
+    private String fileServiceUrl;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Скачать файл")
+    public ResponseEntity<byte[]> getFile(@PathVariable String id) {
+        String url = fileServiceUrl + "/api/files/" + id;
+
+        ResponseEntity<byte[]> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                byte[].class
+        );
+
+        return ResponseEntity.status(response.getStatusCode())
+                .headers(response.getHeaders())
+                .body(response.getBody());
+    }
+
+}
